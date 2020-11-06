afc605316a18e0dc170ad3d6037e205017c79e4f
太木 卿 <QinTaiMu@outlook.com>
2020-11-06 20:06:50 +0800

    错误：org.mybatis.spring。MyBatisSystemException:嵌套异常为org.apache.ibatis.builder。计算表达式'_parameter '错误!= null and_parameter ! =””。原因:org.apache.ibatis.ognl。畸形OGNL表达式:_parameter!= null and_parameter != " [org.apache.ibatis.ognl。ParseException:在第1行第18列遇到" <IDENT> "和_parameter ""。是 关键点：畸形OGNL表达式:_parameter!= null and_parameter != " 解决问题方案：and_parameter 修改为 and _parameter

--- src/test/java/com/xuecheng/manage_course/dao/TestDao.java
+++ src/test/java/com/xuecheng/manage_course/dao/TestDao.java
@@ -1,6 +1,7 @@
 package com.xuecheng.manage_course.dao;
 
 import com.xuecheng.framework.domain.course.CourseBase;
+import com.xuecheng.framework.domain.course.ext.TeachplanNode;
 import com.xuecheng.manage_course.ManageCourseApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,6 +24,9 @@ public class TestDao {
     @Autowired
     CourseMapper courseMapper;
 
+    @Autowired
+    TeachplanMapper teachplanMapper;
+
     @Test
     public void testCourseBaseRepository() {
         Optional<CourseBase> optional = courseBaseRepository.findById("402885816240d276016240f7e5000002");
@@ -38,5 +42,10 @@ public class TestDao {
         CourseBase courseBase = courseMapper.findCourseBaseById("402885816240d276016240f7e5000002");
         System.out.println(courseBase);
 
+    }
+    @Test
+    public void testTeachplanMapper(){
+        TeachplanNode teachplanNode = teachplanMapper.selectList("4028e581617f945f01617f9dabc40000");
+        System.out.println(teachplanNode);
     }
 }
