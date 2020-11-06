fc0ad312b1dd90e77cbee118dc3b3edd885cd0b4
太木 卿 <QinTaiMu@outlook.com>
2020-11-06 19:04:53 +0800

    新增 课程计划 mybatis框架mapper接口 和映射文件mapper.xml

---
+++ src/main/java/com/xuecheng/manage_course/dao/TeachplanMapper.java
@@ -0,0 +1,15 @@
+package com.xuecheng.manage_course.dao;
+
+import com.xuecheng.framework.domain.course.ext.TeachplanNode;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author wuangjing
+ * @create 2020/11/6-18:35
+ * @Description:
+ */
+@Mapper
+public interface TeachplanMapper  {
+
+    public TeachplanNode selectList(String sourceId);
+}
